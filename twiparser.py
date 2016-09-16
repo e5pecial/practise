0374f21445fdff400e0e5e59d771a80b30161ac9
Oleg Chislov <chislov.oleg@yahoo.com>
2016-09-17 02:09:02 +0600

    fastfix

--- twiparser.py
+++ twiparser.py
@@ -25,22 +25,22 @@ def get_person_tweet(person):
         print(e)
 
 def get_mentions_about(person_rus)
-try:
-    tso = TwitterSearchOrder()
-    tso.set_keywords([str(person_rus)])
-    tso.set_language('ru')
-    tso.set_include_entities(False) 
-    ts = TwitterSearch(
-        consumer_key = "",
-        consumer_secret = "",
-        access_token="",
-        access_token_secret=""
-     )
-    file = open('bar_0709_data.json' , 'a')
-    for tweet in ts.search_tweets_iterable(tso):
-        print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
-        json_str = json.dumps(tweet, indent = 4, sort_keys = True)
-        file.write(u''.join(json_str))
-    file.close()
-except TwitterSearchException as e:
-    print(e)
+    try:
+        tso = TwitterSearchOrder()
+        tso.set_keywords([str(person_rus)])
+        tso.set_language('ru')
+        tso.set_include_entities(False) 
+        ts = TwitterSearch(
+            consumer_key = "",
+            consumer_secret = "",
+            access_token="",
+            access_token_secret=""
+         )
+        file = open('bar_0709_data.json' , 'a')
+        for tweet in ts.search_tweets_iterable(tso):
+            print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
+            json_str = json.dumps(tweet, indent = 4, sort_keys = True)
+            file.write(u''.join(json_str))
+        file.close()
+    except TwitterSearchException as e:
+        print(e)
